76ea8dbb9542c0bf0e0a55e84578daad911156fd
CorainChicago <chaysmagan@gmail.com>
2016-06-18 12:29:26 -0500

    make project, create cat file and declare struct with an attribute of name

---
+++ cat.swift
@@ -0,0 +1,12 @@
+//
+//  cat.swift
+//  tableview
+//
+//  Created by Cora Hays-Magan on 6/18/16.
+//  Copyright © 2016 Doublebearrolled. All rights reserved.
+//
+
+import Foundation
+struct Cat{
+    let name : String
+}
