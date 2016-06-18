eece59095ac8d09206e9f1fed33d1b5971c680b1
CorainChicago <chaysmagan@gmail.com>
2016-06-18 12:37:36 -0500

    create an array of cat objects with names

--- tableview/ViewController.swift
+++ tableview/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var cats = [Cat]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.cats = [Cat(name: "Charlie"), Cat(name: "Ella"), Cat(name: "Seabass"), Cat(name: "Cleo")]
     }
 
     override func didReceiveMemoryWarning() {
